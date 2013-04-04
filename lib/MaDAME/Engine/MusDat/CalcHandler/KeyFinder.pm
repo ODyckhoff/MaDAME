03313ac83821fd72fabe4050428ca48777eb85c1
Owen Dyckhoff <owen.dyckhoff@gmail.com>
2013-04-04 02:48:07 +0100

    Major and natural minor key detection working in basic numerical form.

--- lib/MaDAME/Engine/MusDat/CalcHandler/KeyFinder.pm
+++ lib/MaDAME/Engine/MusDat/CalcHandler/KeyFinder.pm
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 
-package MaDAME::Engine::MusDat::KeyFinder;
+package MaDAME::Engine::MusDat::CalcHandler::KeyFinder;
 
 use strict;
 use warnings;
@@ -16,18 +16,68 @@ use warnings;
     #    key - C,  F, Bb, Eb, Ab, Db, Gb, Cb
 
 
-sub obj_get_key { # Detect key based on note objects passed.
-    my @notes = @_; # An array of note objects.
-    foreach $note (@notes) {
-        if $note->is_modified($note) {
-            # It's a sharp or a flat.
+####### REQUIREMENT #######
+#
+# a) either detect key based on numbers of sharps/flats
+#     - problems caused by non black note sharps/flats e.g. E# or Cb
+#
+# b) or based on TTSTTTS pattern for simple Major key calculation
+#     - however, interval data may be needed for relative major/minor calculation
+#     - BUT
+#         + e.g. C major/A minor - C, Dm, Em, F, G, Am, Bdim / Am, Bdim, C, Dm, Em, F, G
+#         + Same thing, different order.
+#         + So just present both sets of information? GetHandler will be able to request single key if user wants it.
+
+
+####### NEW CODE #######
+my @notes = @ARGV;
+print(join(", ", @notes) . "\n");
+#@notes = sort( @notes );
+
+my @possible = ();
+
+
+for( my $root = 0; $root < 12; $root++ ) {
+
+    my @ttsttts = ( 0, 2, 4, 5, 7, 9, 11 );
+    @ttsttts = map { $_ + $root } @ttsttts;
+    foreach(@ttsttts) {
+        if($_ >= 12) {
+            $_ -= 12;
         }
+    }
+   
+    print(join(", ", @ttsttts) . "\n");
+ 
+    my @result = ();
+
+    foreach my $note ( @notes ) {
+        if ($note ~~ @ttsttts) {
+            push(@result, $note);
+        }
+    }
+
+    if( scalar( @result ) == scalar( @notes ) ) {
+        push( @possible, $root );
     }
 }
 
-sub num_get_key { # Detect key based on raw note data (numbers) passed.
-    my @notes = @_; # An array of note numbers.
-    foreach $note (@notes) {
-        
-    }
-}
+print( "The possible keys are " . join( ", ", @possible ) . " major and their relative minors\n" );
+
+####### OLD CODE #######
+#
+# sub obj_get_key { # Detect key based on note objects passed.
+#    my @notes = @_; # An array of note objects.
+#    foreach $note (@notes) {
+#        if $note->is_modified($note) {
+#            # It's a sharp or a flat.
+#        }
+#    }
+#}
+#
+#sub num_get_key { # Detect key based on raw note data (numbers) passed.
+#    my @notes = @_; # An array of note numbers.
+#    foreach $note (@notes) {
+#        
+#    }
+#}
